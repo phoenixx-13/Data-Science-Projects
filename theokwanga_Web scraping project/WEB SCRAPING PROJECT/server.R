06417ab8d8e471bca3c67536a975b093e65770ca
phoenixx-13 <derphoenixx13@gmail.com>
2017-10-25 15:16:53 -0500

    added right ui + server files

--- theokwanga_Web scraping project/WEB SCRAPING PROJECT/server.R
+++ theokwanga_Web scraping project/WEB SCRAPING PROJECT/server.R
@@ -1,191 +1,106 @@
 #SERVER.R
 library(shiny)
+library(shinydashboard)
+library(shinythemes)
 library(data.table)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(gridExtra)
 library(grid)
-library(shinythemes)
-# setwd("~/SHINY_PROJECT/")
-summary(german_credit)
-german_credit_t = read.csv("global/german_credit_T.csv")
-german_credit = read.csv("global/german_credit.csv")
-german_credit_01 = german_credit
-german_credit_01$Purpose = german_credit_t$Purpose
-german_credit_01$Occupation = german_credit_t$Occupation
+library(DT)
+setwd("C:/Users/OLYMPIA/Desktop/WEB SCRAPING PROJECT/")
 
-##
-#Full_summary = summary(German_Credit_01)
-#Sub_summary = summary(german_credit[,c(6,14,3,2)])
-#Sub_summary = data.frame(Sub_summary)
+film_reviews = read.csv("global/film_reviews.csv")
 
-##
-german_credit_by_Age = german_credit_t %>%
-  group_by(Age..years.) %>% 
-  select(Age..years., Credit.Amount) %>%
-  summarise_all(funs(sum))
-
-german_credit_by_Age$Age..years. = factor(german_credit_by_Age$Age..years., levels = german_credit_by_Age$Age..years.[order(-german_credit_by_Age$Credit.Amount)])
-
-#
-german_credit_by_Purpose = german_credit_t %>%
-  group_by(Purpose) %>% 
-  select(Purpose, Credit.Amount) %>%
-  summarise_all(funs(sum))
-
-german_credit_by_Purpose$Purpose = factor(german_credit_by_Purpose$Purpose, levels = german_credit_by_Purpose$Purpose[order(-german_credit_by_Purpose$Credit.Amount)])
-#
-german_credit_by_occupation = german_credit_t %>%
-  group_by(Occupation) %>% 
-  select(Occupation, Credit.Amount) %>%
-  summarise_all(funs(sum))
-##
-german_credit_by_occupation$Occupation = factor(german_credit_by_occupation$Occupation, levels = german_credit_by_occupation$Occupation[order(-german_credit_by_occupation$Credit.Amount)])
-##
-german_credit_by_fam_status = german_credit_t %>%
-  group_by(Sex...Marital.Status) %>% 
-  select(Sex...Marital.Status, Credit.Amount) %>%
-  summarise_all(funs(sum))
-##
-german_credit_by_fam_status$Sex...Marital.Status = factor(german_credit_by_fam_status$Sex...Marital.Status, levels = german_credit_by_fam_status$Sex...Marital.Status[order(-german_credit_by_fam_status$Credit.Amount)])
-##
-
-#### "Group by" graphs
-
-amount_grp_by_age = ggplot(german_credit_by_Age, aes(x = Age..years., y = Credit.Amount, fill = Credit.Amount)) + 
-  geom_bar(stat = "identity") + theme(text=element_text(size=14)) +
-  ggtitle("Credit Amounts grouped by Ages") + 
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_y_continuous(name="Credit.Amount", limits=c(0, 1200000))
-###
-amount_grp_by_purpose = ggplot(german_credit_by_Purpose, aes(x = Purpose, y = Credit.Amount, fill = Credit.Amount)) +
-  geom_bar(stat = "identity", fill = "green") + 
-  ggtitle("Credit Amounts grouped by purpose") +
-  xlab("Purpose") + theme(text=element_text(size=14)) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_y_continuous(name="Credit amount in DM", limits=c(0, 750000))
-###
-amount_grp_by_occupation = ggplot(german_credit_by_occupation, aes(x = Occupation, y = Credit.Amount, fill = Credit.Amount)) +
-  geom_bar(stat = "identity", fill = "darkred") + 
-  ggtitle("Credit amounts grouped by Occupation") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_y_continuous(name="Credit amount in DM") + theme(text=element_text(size=14))
-###
-amount_grp_by_fam_status = ggplot(german_credit_by_fam_status, aes(x = Sex...Marital.Status, y = Credit.Amount, fill = Credit.Amount)) +
-  geom_bar(stat = "identity", fill = "red") + 
-  ggtitle("Credit amounts grouped by family status") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_y_continuous(name="Credit amount in DM") + theme(text=element_text(size=14))
-### Credit default graphs
-default_by_age_grp =  ggplot(german_credit_t, aes(x = reorder(Age..years., -Creditability), fill = factor(Creditability))) +
-  geom_bar(position = "dodge") + xlab("Age..years.") + guides(fill=guide_legend(title="Creditability")) +
-  ggtitle("Credit default proportion grouped by Ages") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_fill_hue(labels=c("Not credible", "Credible")) + theme(text=element_text(size=14))
-###
-default_by_purpose =  ggplot(german_credit_t, aes(x = reorder(Purpose, -Creditability), fill = factor(Creditability))) +
-  geom_bar(position = "dodge") + guides(fill=guide_legend(title="Creditability")) +
-  ggtitle("Credit default proportion grouped by purpose") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_fill_hue(labels=c("Not credible", "Credible")) + theme(text=element_text(size=14))
-###
-default_by_occupation =  ggplot(german_credit_t, aes(x = reorder(Occupation, -Creditability), fill = factor(Creditability))) +
-  geom_bar(position = "dodge") + guides(fill=guide_legend(title="Creditability")) +
-  ggtitle("Credit default proportion grouped by occupation") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_fill_hue(labels=c("Not credible", "Credible")) + theme(text=element_text(size=14))
-###
-default_by_fam_status =  ggplot(german_credit_t, aes(x = reorder(Sex...Marital.Status, -Creditability), fill = factor(Creditability))) +
-  geom_bar(position = "dodge") + guides(fill=guide_legend(title="Creditability")) +
-  ggtitle("Credit default proportion grouped by family status") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  scale_fill_hue(labels=c("Not credible", "Credible")) + theme(text=element_text(size=14))
-###
-# An if else statement is used to determine which data subset (grouping) to be displayed.
-
-server <- function(input, output,session) {
-  
-  output$plot01 <- renderPlot({plot(1:10 ,1:10)
-    
-    if(input$plot_var1=="Age"){
-      amount_grp_by_age 
+server <- function(input, output) {
+ 
+  output$plot01 <- DT::renderDataTable({
+    film_reviews[,c(1,2,3,4,5,11,12,7)][1:8,]
+})
+  output$plot2 <- renderPlot({
+     
+    if(input$eda_graph=="Duration"){
+      
+      hist(film_reviews$duration, main="Histogram for duration of film", 
+           xlab= "Duration in minutes", col="green")
       
     }else{
       
-      if(input$plot_var1=="Purpose"){
+      if(input$eda_graph=="Release month"){
         
-        amount_grp_by_purpose  
+        ggplot(film_reviews, aes(x = release_month))  + geom_histogram(fill="blue", bins = 36) 
+         
+      }else{
+      
+      if(input$eda_graph=="Release year"){
+        
+      ggplot(film_reviews01, aes(x = release_year, group=rating, fill=rating)) + geom_histogram(bins = 20) +
+        stat_count() + theme(axis.text.x  = element_text(angle=45, vjust=0.5, size=8))
         
       }else{
         
-        if(input$plot_var1=="Occupation"){
+        if(input$eda_graph=="Rating"){
           
-          amount_grp_by_occupation 
+          ggplot(film_reviews, aes(x = rating))  + geom_histogram(fill="blue") 
           
         }else{
           
-          if(input$plot_var1=="Family status"){
-            
-            amount_grp_by_fam_status 
-          }
-        }}}
-    
-    
-  }) #height = 400, width = 600)  
-  
-  output$plot02 <- renderPlot({plot(1:10 ,10:1)
-    
-    
-  
-      if(input$plot_var2=="Age"){
-        default_by_age_grp
-
-      }else{
-
-        if(input$plot_var2=="Purpose"){
-
-          default_by_purpose
-
-        }else{
-
-          if(input$plot_var2=="Occupation"){
-
-            default_by_occupation
-
+          if(input$eda_graph=="Duration vs release date"){
+          
+          qplot(x = release_date, y = duration, colour = rating, xlab="Release date", ylab="Duration", data = film_reviews01)
+         
           }else{
-
-            if(input$plot_var2=="Family status"){
-
-              default_by_fam_status
-            }
-          }}}
-
-  })
-  
-  # The section below uses the user imputs to generate a scatterplot
-  
-  output$var_scatterplot <- renderPlot({
-  
-    filtered_data =  german_credit_01 %>%
-        filter(., (Age..years. >= input$Age_input[1] & Age..years. <= input$Age_input[2]),
-               Occupation == input$Occup_input,
-               (Credit.Amount >= input$Amount_input[1] & Credit.Amount <= input$Amount_input[2])
-        )  
-        
-      ggplot(filtered_data, aes(y= Credit.Amount, x=Age..years.)) + geom_point(aes(colour = factor(Creditability)),  size = 3) +
-      geom_smooth(method=lm) + theme(text=element_text(size=14))
-    })
-# position = position_jitterdodge()
-  # Generate a subset of the data base on user selections 
-  
-  output$data_subset <- renderTable({
-    
-    filtered_data =  german_credit_01 %>%
-        filter(., (Age..years. >= input$Age_input[1] & Age..years. <= input$Age_input[2]),
-               Occupation == input$Occup_input,
-               (Credit.Amount >= input$Amount_input[1] & Credit.Amount <= input$Amount_input[2])
-        )  
-    filtered_data = filtered_data[,c(1,6,14)] 
-    
-    head(summary(filtered_data), 4) # Print the fist 4 lines in the filtered_data summary
+            
+            if(input$eda_graph=="Duration vs release year"){
+              
+              qplot(x = release_year, y = duration, colour = rating, xlab="Release year", ylab="Duration", data = film_reviews01)
+          
+          
+        }else{
+          
+          if(input$eda_graph=="Sentiment"){
+            
+          
+          ggplot(film_reviews, aes(x = sentiment)) + geom_histogram(stat = "count", fill="blue") 
+             
+          }else{
+            
+         if(input$eda_graph=="Certificate"){
+              
+         
+         ggplot(film_reviews, aes(x = certificate)) + geom_histogram(stat = "count", fill="blue")    
+          
+       }}}}}}}}
     
   })
-
+  
+  film_reviews01 =subset(film_reviews, film_reviews$release_year > "1975")
+  
+  output$plot2b <- renderPlot({
+    ggplot(film_reviews01, aes(x = release_year, group=rating, fill=rating)) + geom_histogram(stat = "count") +
+      theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=8))
+    
+  })
+  
+  output$plot2c <- renderPlot({
+  
+  ggplot(film_reviews01, aes(x = release_year, group=certificate, fill=certificate)) + geom_histogram(stat = "count")+
+    theme(axis.text.x  = element_text(angle=90, vjust=0.5, size=8))
+  })
+  output$plot3 <- renderPlot({
+  
+      ggplot(film_reviews, aes(x = duration, group=rating, fill=rating)) +
+    
+      geom_histogram(data=subset(film_reviews, (film_reviews$duration > input$dur[1] &  film_reviews$duration < input$dur[2])))
+    
+  })
+  
+  output$data01 <- DT::renderDataTable({
+    film_reviews[,c(1,2,3,4,5,11,12,7,8)]
+    
+  })
 }
+  
+ 
+
--- theokwanga_Web scraping project/WEB SCRAPING PROJECT/ui.R
+++ theokwanga_Web scraping project/WEB SCRAPING PROJECT/ui.R
@@ -1,79 +1,54 @@
-##
-library(shiny)
-library(shinythemes)
+## app.R ##
 
-fluidPage(theme = shinytheme("superhero"),  
-  titlePanel("German Credit Data Visualization by Theo Kwanga (NYCDSA)"),
-             sidebarLayout(
-             sidebarPanel(
-              
-             # Using a conditional panel to display different sidebars for different tabs
-               
-              conditionalPanel(condition="input.conditionedPanels==1",
-                    tags$div(class="header", checked=NA,
-                    tags$p("This is a shiny application to visualize German credit data and highlight
-                            major creditors and credit default trends which can be further investigated in 
-                            order to improve lending services to identified groups and also mitigate credit defaulting."),
-                    div(style = "height:25px;width:100%;background-color: #999999"),                 
-                    tags$p("Caution: The statistical significance of the visual inferences has not been considered nor
-                            analysed at this stage")),
-                    
-                    div(style = "height:25px;width:100%;background-color: #999999"),   
-                        
-                    radioButtons("plot_var1", h4("Select variable to view credit trend:"), selected = "Age",
-                    list("Age", "Purpose", "Occupation", "Family status"))
-              
-             ),
-               
-               conditionalPanel(condition="input.conditionedPanels==2",
-                 radioButtons("plot_var2", h4("Select variable to view credit trend:"), selected = "Age",
-                    list("Age", "Purpose", "Occupation", "Family status"))),
-                        
-              
-              conditionalPanel(condition="input.conditionedPanels==3",
-                               
-                               sliderInput("Age_input", h4("Enter Age range"), 20, 70, c(25, 35),
-                               animate = animationOptions(interval = 1000, loop = TRUE)),
-                                
-                         sliderInput("Amount_input", "Enter Credit range", 0, 20000, c(0, 5000),step = 2500, post = "DM"),
-              
-                 radioButtons("Occup_input", "Choose Occupation",
-                           choices = c("Highly qualified employee", "Skilled employee", "Unemployed/unskilled", "Unskilled"),
-                           selected = "Unskilled"))
-                              
-                               ),
-                              
-             mainPanel(
-                 
-                 tabsetPanel(
-                   
-                  tabPanel("Grapical EDA", value=1,
-                          
-                  plotOutput("plot01")),
-                 
-                  
-    
-                  tabPanel("Credit default proportions", value=2, #fluid = TRUE,
-                           
-                 # fluidRow(verticalLayout(splitLayout(cellWidths = c("50%", "50%"))),
-                           
-                        #  column(10, offset=2,
-                           
-                           #plotOutput("plot01"),
-                           plotOutput("plot02")),
-                 
-                 tabPanel("Credit default distributions", value=3,  fluid = TRUE,
-                          plotOutput("var_scatterplot"),
-                          tableOutput("data_subset")),
-                 
-                 id = "conditionedPanels"
-
-
-       )
+ui <- dashboardPage(skin = "blue",
+      
+  dashboardHeader(title = "Web scraping Project"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Main Page", tabName = "mainpage", icon = icon("mainpage")),
+      menuItem("EDA Graphs", tabName = "eda", icon = icon("eda")),
+      menuItem("Duration vs release year", tabName = "sims", icon = icon("sim")),
+      menuItem("Dataset", tabName = "data", icon = icon("data"))
     )
-  ) 
+  ),
+  
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "mainpage",
+              fluidRow(
+                    box(h2("Brief dataset overview"),
+                    DT::dataTableOutput("plot01"))
+                
+                )
+              ),    
+           
+      tabItem(tabName = "eda",
+              box(plotOutput("plot2", height = 500)),
+              
+              box(
+              radioButtons(inputId = "eda_graph", "Choose variable to see graph:",
+                           list("Duration", "Release month", "Release year", "Rating", "Sentiment",
+                                "Duration vs release date","Duration vs release year","Certificate")))
+            
+           ),
+      tabItem(tabName = "sims",
+              fluidRow(
+                box(plotOutput("plot3", height = 500)),
+                box(
+                  
+               
+                 sliderInput("dur", h4("Duration vs rating"), 0, 400, c(60, 90)))
+              )   
+      ),  
+      
+      tabItem(tabName = "data",
+             
+              h2("Film review complete dataset"),
+             
+              box(DT::dataTableOutput("data01"))
+              
+             )
+      
+    )
+  )
 )
-
-
-
-
